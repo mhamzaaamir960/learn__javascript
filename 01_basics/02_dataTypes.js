69ea0bbe08751029761feb1b3762bb3e58ff12e3
hamzaaamir <hamzabhatti782259@gmail.com>
2023-08-09 15:44:39 +0500

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat al js code in new version
+
+// alert (3+3) // we are using browser not js
+
+
+console.log( 3 
+    + 
+    3) // code readability should be high
+
+console.log(3+3);
+
+let name = "hamza";
+let age = 19;
+let is_loged_in = true;
+let state;
+
+// Data Types
+// number => 2 to power 53
+// bigint
+// string => ""
+// null => stand alone value
+// undefined
+// symbol => unique
+
+console.log(typeof age);  // number data type 
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined 
