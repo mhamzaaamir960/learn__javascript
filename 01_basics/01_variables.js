a2881ae2f2ffd04009d34c061d959e6d94348c8b
hamzaaamir <hamzabhatti782259@gmail.com>
2023-08-09 14:48:13 +0500

    added variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const { log, table } = require("console");
+
+const account_id = 14413;
+let account_email = "hamza@gmail.com";
+var account_password = "213910";
+account_city = "Rawalpindi";
+let account_state;
+
+
+// account_id = 85943; not allowed
+
+console.log(account_id);
+
+/* prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+account_email = "hamza1122@gmail.com";
+account_password = "938191";
+account_city = "Islamabad"
+
+console.table([account_id,account_email,account_password,account_city,account_state]);
